7c75501869f81df80f819fc551157f906dad9f7a
Nathan Feng <nathanlfeng@gmail.com>
2018-10-18 10:11:23 -0400

    added redux

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+
+  state = {
+    input: '',
+  }
+
+  onFieldChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.
+
+  }
+
   render() {
     return(
-      <h3>Enter the Symbol for the Coins you want to track</h3>
+      <div>
+        <h3>Enter the Symbol for the Coins you want to track</h3>
+        <form onSubmit={this.onSubmit}>
+          <input
+            placeholder='Coin Symbol'
+            type='text'
+            onChange={this.onFieldChange}
+          /> <br/>
+          <input type="checkbox" value="Bike"/> I hold this coin <br/>
+          <button>Submit</button>
+        </form>
+      </div>
+
     )
   }
 }
